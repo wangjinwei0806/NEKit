0d012a3788b10299ef91a663f5cb4484b701205d
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-16 10:56:06 +0800

    Add scripts to build and push doc

---
+++ scripts/build_docs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+gem install jazzy --no-ri --no-rdoc
+
+git submodule update --init --recursive --remote --merge
+
+jazzy --swift-version 2.2 \
+      -o ./ \
+      --source-directory NEKit/ \
+      --readme NEKit/README.md \
+      --author "Zhuhao Wang" \
+      --module "NEKit" \
+      --github_url "https://github.com/zhuhaow/NEKit"
+
