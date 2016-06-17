23f2fa7d8f204b8527edfe606ebc1e1d7e83f548
Zhuhao Wang <zhuhaow@users.noreply.github.com>
2016-06-17 21:39:12 +0800

    Remove command line config

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -5,12 +5,4 @@ gem install jazzy --no-ri --no-rdoc
 rm -rf tun2socks
 git submodule update --init --remote --merge
 
-jazzy --swift-version 2.2 \
-      -o ./ \
-      --source-directory NEKit/ \
-      --readme NEKit/README.md \
-      --author "Zhuhao Wang" \
-      --module "NEKit" \
-      --github_url "https://github.com/zhuhaow/NEKit" \
-      --min-acl internal
-
+jazzy
